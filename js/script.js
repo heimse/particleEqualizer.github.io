d64472f5d9321c0130368d593d48379db493d172
heimse <heimseweb@gmail.com>
2023-08-02 06:09:06 +0300

    csv handling

--- js/script.js
+++ js/script.js
@@ -187,7 +187,7 @@ function readCSVFile(){
             const charts = document.querySelectorAll('.chartJs');
 
             const date_past = [];
-            date_past = createArrayOfIndex(array, 3).shift()
+            date_past = getAllColumn(array, 3).shift()
             console.log(array);
             console.log(date_past);
 
