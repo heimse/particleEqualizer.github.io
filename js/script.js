21c72fbca561a1e4713d7b21356408bc896c07c9
heimse <heimseweb@gmail.com>
2023-08-02 06:30:05 +0300

    csv handling

--- js/script.js
+++ js/script.js
@@ -232,24 +232,7 @@ function readCSVFile(){
 }
 function drawChart(id, settings) {
     const ctx = $(`#myChart${id}`);
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
+    new Chart(ctx, settings);
 }
 
 
